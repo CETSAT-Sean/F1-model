6cc0f0b620603201b7332aa64d028eefa98b195d
mrsean2005 <141424732+mrsean2005@users.noreply.github.com>
2026-01-27 19:50:54 +0530

    fixed wireless communication

--- F1-model/src/main.cpp
+++ F1-model/src/main.cpp
@@ -1,27 +1,58 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_now.h>
+uint8_t rec[6] = {0x5C, 0x01, 0x3B, 0x47, 0x5F, 0x84};
 
-// put function declarations here:
-int myFunction(int, int);
-
+typedef struct msg{
+    char 
+    data[32];
+    int throttle;
+}msg;
+msg message;
+esp_now_peer_info_t peerInfo;
+void OnDatasent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+    Serial.print("Last Packet Send Status: ");
+    Serial.println(String(status));
+    if(status == ESP_NOW_SEND_SUCCESS){
+        Serial.println("Delivery Success");
+    }
+    else{
+        Serial.println("Delivery Fail");
+    }
+}
+//upload this
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
   Serial.begin(115200);
-  Serial.print("The result is: ");
-  pinMode(LED_BUILTIN, OUTPUT);
-
+  WiFi.mode(WIFI_STA);
+  esp_err_t status = esp_now_init();
+    if (status != ESP_OK) {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+    else {
+        Serial.println("Transmission beginning");
+    }
+    memcpy(peerInfo.peer_addr, rec, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+    if(esp_now_add_peer(&peerInfo) != ESP_OK){
+        Serial.println("Failed to add peer");
+        return;
+    }
+    esp_now_register_send_cb(OnDatasent);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:  
-    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making
-  Serial.println("Hello, world!");
-  delay(1000);                       // wait for a second
-}
+void loop(){
+    memcpy(message.data,"the transmission is on going",sizeof(message));
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    message.throttle=1234;
+    esp_err_t result = esp_now_send(rec, (uint8_t *) &message,sizeof(message));
+    if (result == ESP_OK) {
+        Serial.println("Message sent");
+    }
+    else {
+        Serial.println("Error sending the message");
+    
+    }
+    delay(2000);
+
 }
